70eac6b436f0045026c5f45a54553ceafdc4f208
RomaSamkov <Romansamkov.rs@gmail.com>
2023-08-24 20:32:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 import { Grid } from "./grid.js";
+import { Tile } from "./tile.js";
+import { Cell } from "./cell.js";
+
+console.log(Cell.linkTile);
 
 const gameBoard = document.getElementById("game-board");
 
 const grid = new Grid(gameBoard);
+grid.getRandomEmptyCell().linkTile(new Tile(gameBoard));
