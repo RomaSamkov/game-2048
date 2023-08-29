b9becf60f2b607c8dd84b84266c72d4bad503292
RomaSamkov <Romansamkov.rs@gmail.com>
2023-08-29 20:23:47 +0300

    Update tile.js

--- tile.js
+++ tile.js
@@ -27,4 +27,20 @@ export class Tile {
   removeFromDOM() {
     this.tileElement.remove();
   }
+
+  waitForTransitionEnd() {
+    return new Promise((resolve) => {
+      this.tileElement.addEventListener("transitionend", resolve, {
+        once: true,
+      });
+    });
+  }
+
+  waitForAnimationEnd() {
+    return new Promise((resolve) => {
+      this.tileElement.addEventListener("animationend", resolve, {
+        once: true,
+      });
+    });
+  }
 }
